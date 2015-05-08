e8ae6f8d798142259d7e6ffed20b22bffbdacc6f
Alessandro Lai <alessandro.lai85@gmail.com>
2015-05-08 16:16:40 +0200

    Up to 6 buildleaders!

--- source/scripts/project/controllers.js
+++ source/scripts/project/controllers.js
@@ -21,7 +21,7 @@ angular.module( "wall.controllers", [] )
 
         $scope.maxRepos   = 8;
         $scope.maxBuilds  = 5;
-        $scope.maxLeaders = 4;
+        $scope.maxLeaders = 6;
 
         $scope.now = moment();
         var pollTime = null;
